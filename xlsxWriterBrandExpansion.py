e8f661689d42a6b0c7a0619a962baf26ef9d14bd
Jonathan Shek <jonathanJJShek@gmail.com>
2018-09-18 11:34:37 -0700

    changed column range

--- xlsxWriterBrandExpansion.py
+++ xlsxWriterBrandExpansion.py
@@ -14,7 +14,7 @@ import pandas as pd
 ### READ CSV 
 
 # Match filename
-f = "Spark Agent List Match_2018-09-10_130859_output.csv"
+f = "DigitalGlobe-Addtl Ads Accounts No Geo Restrictions_2018-09-18_131574_output.csv"
 
 # Create dataframe for entire match file to obtain number of columns(length).
 matchfile_df = pd.read_csv(f, skiprows=None)
@@ -22,10 +22,10 @@ length = len(matchfile_df.columns)
 
 # Select column ranges to include:
 # Source Name, Match Result, Match Type, Best SID, Account, Country Code, MEI
-range1 = [0,1,2,4,7,8,12,13,22,23,30]
+range1 = [0,1,2,4,7,8,12,13,17,22,23,24,30]
 
 # Range for all custom attribute columns
-range2 = range(37,length)
+range2 = list(range(37,length))
 
 # List of columns to include
 cols = range1 + range2
@@ -42,13 +42,16 @@ df = pd.read_csv(f, skiprows=None, usecols=cols)
 
 # Create 'reachable' dataframe for 'MEI' >= 25 and 'active' == True
 reachable_df = df[(df['MEI'] >= 25) & (df['active'] == True)]
+reachable_df = reachable_df.sort_values(by=['MEI'], ascending=False)
 
 # Create 'Not_Reachable' dataframe for 'MEI' < 25 and 'active' == True
 not_reachable_df = df[(df['MEI'] < 25) & (df['active'] == True)]
+not_reachable_df = not_reachable_df.sort_values(by=['MEI'], ascending=False)
 
 # Create 'No_Match' dataframe for rows that require manual scrubbing
 # These rows also have 'active' == False and do not contain any duplicates
 no_match_df = df[(df['Match Result'] == 'no match') & (df['Match Type'] != 'duplicate input') & (df['Match Type'] != 'duplicate match')]
+no_match_df = no_match_df.sort_values(by=['MEI'], ascending=False)
 
 # Create 'Duplicates' dataframe of rows with duplicate input or duplicate match
 duplicates_df = df[((df['Match Type'] == 'duplicate input') | (df['Match Type'] == 'duplicate match'))]
@@ -57,7 +60,7 @@ duplicates_df = df[((df['Match Type'] == 'duplicate input') | (df['Match Type'] 
 # Drop unwanted columns in each dataframe
 reachable_df = reachable_df.drop(columns = ['active', 'Match Type', 'Match Result'])
 not_reachable_df = not_reachable_df.drop(columns = ['active', 'Match Type', 'Match Result'])
-no_match_df = no_match_df.drop(columns = ['active', 'Match Type', 'Match Result', 'Best SID', 'Account', 'Country Code', 'Country Name'])
+no_match_df = no_match_df.drop(columns = ['active', 'Match Type', 'Match Result'])
 duplicates_df = duplicates_df.drop(columns = ['active', 'Match Type', 'Match Result'])
 
 #----------------------------------------------------------------------------------------------------------------------
@@ -65,7 +68,7 @@ duplicates_df = duplicates_df.drop(columns = ['active', 'Match Type', 'Match Res
 ### WRITE XLSX
 
 # Excel filename
-f2 = 'Spark Agent List Match_2018-09-10_130859_output.xlsx'
+f2 = 'DigitalGlobe-Addtl Ads Accounts No Geo Restrictions_2018-09-18_131574_output.xlsx'
 
 writer = pd.ExcelWriter(f2, engine = 'xlsxwriter')
 reachable_df.to_excel(writer, sheet_name = 'Reachable', index = False)
